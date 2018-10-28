b3d15b1b257d36a7a6133a3e5a53db90f6953dae
oimo23 <oimo2338@gmail.com>
2018-10-28 10:44:20 +0900

    APIが呼び出せない問題を修正

--- src/domain/Geocoder.js
+++ src/domain/Geocoder.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
+const GEOCODE_API_KEY  = 'AIzaSyCTEQ2mmPsaDmw1PJ9DnsPz4sqOOUoqFCw';
 
 export const geocode = place =>
   axios
-    .get(GEOCODE_ENDPOINT, { params: { address: place } })
+    .get(GEOCODE_ENDPOINT, { params: { address: place, key: GEOCODE_API_KEY } })
     .then((results) => {
       const data = results.data;
       const status = data.status;
